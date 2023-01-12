fbc88834cafb385745456aba7b92dc13fca405ae
tangxf <547369334@qq.com>
2023-01-12 16:58:12 +0800

    fix: te

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,12 +17,6 @@ const count = ref(0);
   </div>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
     Install
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
